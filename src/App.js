fadefdfec924509b737698bfe2c9db749680e91a
Connor Lagana <connor.lagana@gmail.com>
2019-12-12 11:02:37 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  onChange = (e) => {
+    const content = e.target.value
+
+    this.setState({
+      content: content
+    })
+
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <form onSubmit={this.postSubmit}>
+          <input
+            type="textarea"
+            name="content"
+            value={this.state.content}
+            placeholder="POST IT"
+            onChange={this.onChange}
+          />
+
+          <input
+            type="submit"
+            name="content"
+            value="Post"
+            placeholder="POST IT"
+            onChange={this.onChange}
+          />
+        </form>
+      </div>
+    );
+  }
+
 }
 
 export default App;
